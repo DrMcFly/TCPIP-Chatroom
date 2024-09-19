3fd703b9bffc0afa1e2ce1b7e18e0007ce1728d6
DrMcFly <xmartin.mccorklex@gmail.com>
2024-09-19 11:58:03 -0500

    updates

--- client.c
+++ client.c
@@ -5,10 +5,8 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define IP "192.168.50.17"
-#define PORT 2005
-
-// uint8_t *DEVICE_ID = 0001;
+#define IP "127.0.0.1"
+#define PORT 2006
 
 int main() {
     char *buffer = (char *)malloc(4026);
@@ -16,49 +14,86 @@ int main() {
     struct sockaddr_in server;
     int lfd;
 
-    // Create socket
-    lfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (lfd < 0) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up server address
-    server.sin_family = AF_INET;
-    server.sin_port = htons(PORT);  // Change port to 2006
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-
     // Connect to server
-    if (connect(lfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        perror("connect");
-        close(lfd);
-        exit(EXIT_FAILURE);
-    }
+    // if (connect(lfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+    //     perror("connect");
+    //     close(lfd);
+    //     exit(EXIT_FAILURE);
+    // }
+
+    system("clear");
 
     int numchars = 15;
-    char name[] = "Martin";
+    char name[] = "First";
     // printf("%ld", strlen(name));
     size_t data_length = (strlen(name) > numchars) ? numchars : strlen(name);
-    memmove(buffer, name, data_length);
+    memmove(buffer + 1, name, data_length);
 
-    char lname[] = "McCorkle";
+    char lname[] = "Last";
     data_length = (strlen(lname) > numchars) ? numchars : strlen(lname);
-    memmove(buffer + numchars, lname, data_length);
-    
+    memmove(buffer + numchars + 1, lname, data_length);
+
+    char *user_flag = "1";
+    memmove(buffer, user_flag, 1);
+
     // Print the first part (up to the first name)
-    printf("First name in buffer: %.*s\n", (int)strlen(name), buffer);
+    // printf("First name in buffer: %.*s\n", (int)strlen(name), buffer);
 
     // Print the second part (starting from where the last name is inserted)
-    printf("Last name in buffer: %.*s\n", (int)strlen(lname), buffer + numchars);
+    // printf("Last name in buffer: %.*s\n", (int)strlen(lname), buffer + numchars);
 
-    printf("Attempting send...");
-    send(lfd, buffer, 4026, 0);
+    int m_length = 500;
+
+    while(1) {
+        
+        // Create socket
+        lfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (lfd < 0) {
+            perror("socket");
+            exit(EXIT_FAILURE);
+        }
+
+        // Set up server address
+        server.sin_family = AF_INET;
+        server.sin_port = htons(PORT);  // Change port to 2006
+        server.sin_addr.s_addr = inet_addr(IP);
+
+        if (connect(lfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+            perror("connect");
+            close(lfd);
+            exit(EXIT_FAILURE);
+        }
+        
+        memset(buffer + 31, 0, m_length);
+        char message[m_length];
+
+        fgets(message, sizeof(message), stdin);
+
+        data_length = (strlen(message) > m_length) ? m_length : strlen(message);
+        memmove(buffer + 30, message, data_length);
+
+        // printf("Buffer contents (%zu bytes):\n", data_length);
+        
+        char *keyword = ":exit";
+        if (strstr(buffer + 30, keyword) != NULL) {
+            printf("Closing connection...\n");
+            close(lfd);
+            system("clear");
+            return 0;
+        }
+
+        send(lfd, buffer, 4026, 0);
+
+        system("clear");
+
+        // close(lfd);
+
+    }
+
     free(buffer);
-    printf("Sent!\n");
-
     gets();
 
-    send(lfd, "", 0, 0);
+    // send(lfd, "", 0, 0);
     close(lfd);
 
     return 0;
